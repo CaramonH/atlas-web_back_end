29ffd30f985c61d8188d0c4ecf87d7e1f2d4197c
CaramonH <caramonh@yahoo.com>
2024-01-02 14:09:26 -0600

    Task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+""" function floor that takes a float n as an argument and returns the floor"""
+import math
+
+
+def floor(n: float) -> int:
+    """Return the floor of a floating-point number."""
+    return math.floor(n)
