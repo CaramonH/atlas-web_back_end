cd4507060484a68b7945573de8a98edbea16bf7e
CaramonH <caramonh@yahoo.com>
2024-01-02 14:14:09 -0600

    Task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""function to_str that takes a float n as argument and returns a string"""
+
+
+def to_str(n: float) -> str:
+    """Return the string representation of a floating-point number"""
+    return str(n)
