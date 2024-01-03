d0d8422d0172679e64865a32d5e6c5e3945f7502
CaramonH <caramonh@yahoo.com>
2024-01-03 14:00:03 -0600

    Task 2 - Tridux

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
-"""Function measure_time with integers n and max_delay as arguments that
-measures the total execution time for wait_n(n, max_delay), and returns
-total_time / n"""
+"""From the previous file, import wait_random into 2-measure_runtime.py.
+Create a measure_time function with integers n and max_delay as arguments
+that measures the total execution time for wait_random(n, max_delay), and
+returns total_time / n. Your function should return a float."""
+
+
 import asyncio
 import time
 from typing import List
