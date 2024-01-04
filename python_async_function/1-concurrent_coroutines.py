91cbcb9e056f3cfc769698fb54f32872a574f166
CaramonH <caramonh@yahoo.com>
2024-01-04 13:07:01 -0600

    Testing #1 for Evan

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
-"""async routine called wait_n that takes in 2 int arguments
- (in this order): n and max_delay"""
-import asyncio
+"""returns list of random wait times"""
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int = 10) -> List[float]:
-    """calls coroutine and returns sorted list"""
-    delays = [await wait_random(max_delay) for _ in range(n)]
-    return sorted(delays)
+async def wait_n(n: int, max_delay: int = 10) -> List:
+    """
+    Args:
+        n (int): _description_
+        max_delay (int, optional): _description_. Defaults to 10.
+    Returns:
+        List: _description_
+    """
+    delay_list = [await wait_random(max_delay) for _ in range(n)]
+    return sorted(delay_list)
