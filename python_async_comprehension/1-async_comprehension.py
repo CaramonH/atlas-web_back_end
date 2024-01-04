a1dfebcc6e78cefd76941e541fe4fc3f9f5157e8
CaramonH <caramonh@yahoo.com>
2024-01-04 12:41:43 -0600

    Task 1 actually this time

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 
-""""""
+"""Coroutine called async_comprehension that takes no arguments.
+    The coroutine will collect 10 random numbers with aync and return them."""
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """coroutine that uses async comprehension to collect 10 random numbers"""
+    return [i async for i in async_generator()]
