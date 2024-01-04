33ae8f209fee1f48546a5125ae9a86dd78add987
CaramonH <caramonh@yahoo.com>
2024-01-04 12:53:10 -0600

    Task 2

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 
-""""""
+"""Coroutine that executes async_comprehension four times in paralell
+    using async.gather"""
+import asyncio
+from typing import List
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """"""
+    start_time = asyncio.get_event_loop().time()
+
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
+
+    end_time = asyncio.get_event_loop().time()
+    total_runtime = end_time - start_time
+    return total_runtime
