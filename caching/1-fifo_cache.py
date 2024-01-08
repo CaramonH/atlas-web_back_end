fc7961bda9226fd0e2f32ee803c56fdf01d9c6a9
CaramonH <caramonh@yahoo.com>
2024-01-08 12:08:32 -0600

    Task 1

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
-""""""
+""" 1-fifo_cache.py """
+BaseCaching = __import__('base_caching').BaseCaching
 
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache class """
+
+    def __init__(self):
+        """ Initialize FIFOCache instance with parent init  """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item to the cache """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discarded_key = next(iter(self.cache_data))
+                del self.cache_data[discarded_key]
+                print("DISCARD:", discarded_key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Retrieve an item from the cache """
+        return self.cache_data.get(key, None)
