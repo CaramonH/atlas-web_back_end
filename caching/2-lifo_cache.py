98ae2f0374328daab9a40b29b0d7ff4e4c322c0e
CaramonH <caramonh@yahoo.com>
2024-01-08 12:43:18 -0600

    Task 2

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
-""""""
+""" 2-life_cache """
+BaseCaching = __import__('base_caching').BaseCaching
 
+
+class LIFOCache(BaseCaching):
+    """ LIFOCache class """
+
+    def __init__(self):
+        """ Initialize LIFOCache instance with parent init """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item to the cache """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # get the last key and remove it from the cache
+                discarded_key = list(self.cache_data.keys())[-1]
+                del self.cache_data[discarded_key]
+                print("DISCARD:", discarded_key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Retrieve an item from the cache """
+        return self.cache_data.get(key, None)
