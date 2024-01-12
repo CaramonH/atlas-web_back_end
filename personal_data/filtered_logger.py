579d6a94f3536103497e487684156dfb525f8f9c
CaramonH <caramonh@yahoo.com>
2024-01-12 12:43:48 -0600

    Task 0 ?

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,10 +6,12 @@ function filter_datum that returns the log message obfuscated
 """
 
 import re
-from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields, redaction, message, separator):
     """Obfuscate certain fields in a log message."""
-    pattern = f'(?<={separator}){"|".join(fields)}(?={separator})'
-    return re.sub(pattern, redaction, message)
+    return re.sub(
+        r'(' + '|'.join(fields) + r')=[^' + re.escape(separator) + r']*',
+        lambda match: match.group(1) + '=' + redaction,
+        message
+    )
