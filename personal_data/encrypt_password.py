8ae79f100cde04542e67d1d512419c8b9e7974d9
CaramonH <caramonh@yahoo.com>
2024-01-12 14:31:48 -0600

    Task 5

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
-""""""
+"""
+encrypted_password
 
+function hash_password that hashes a password using bcrypt
+"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Hash a password using bcrypt with salt."""
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    return hashed_password
