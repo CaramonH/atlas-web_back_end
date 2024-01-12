12844344dd46d1630851679d8407af616aa59260
CaramonH <caramonh@yahoo.com>
2024-01-12 14:44:54 -0600

    parameter annotation

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -17,6 +17,6 @@ def hash_password(password: str) -> bytes:
     return hashed_password
 
 
-def is_valid(hashed_password: bytes, password: str):
+def is_valid(hashed_password: bytes, password: str) -> bool:
     """Check if the provided password matches the hashed password."""
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
