cff7be2a407dbdf166679ef63156d5458994fd50
CaramonH <caramonh@yahoo.com>
2024-01-15 12:20:33 -0600

    users

--- Basic_authentication/api/v1/views/users.py
+++ Basic_authentication/api/v1/views/users.py
@@ -1,36 +1,27 @@
 #!/usr/bin/env python3
 """ Module of Users views
 """
-from api.v1.views import app_views
+from api.v1.auth.auth import Auth  # Import your Auth class
 from flask import abort, jsonify, request
+from api.v1.views import app_views
 from models.user import User
 
+auth = None  # Instantiate your Auth class based on the AUTH_TYPE environment variable
 
-@app_views.route('/users', methods=['GET'], strict_slashes=False)
+@app_views.route('/api/v1/users', methods=['GET'], strict_slashes=False)
 def view_all_users() -> str:
     """ GET /api/v1/users
     Return:
       - list of all User objects JSON represented
     """
+    if auth and auth.require_auth(request.path, ['/api/v1/users']):
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
+
     all_users = [user.to_json() for user in User.all()]
     return jsonify(all_users)
-
-
-@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
-def view_one_user(user_id: str = None) -> str:
-    """ GET /api/v1/users/:id
-    Path parameter:
-      - User ID
-    Return:
-      - User object JSON represented
-      - 404 if the User ID doesn't exist
-    """
-    if user_id is None:
-        abort(404)
-    user = User.get(user_id)
-    if user is None:
-        abort(404)
-    return jsonify(user.to_json())
 
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
