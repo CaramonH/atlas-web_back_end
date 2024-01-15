4768f92a02fdecbf4c6bdfc78531cf3f0e830528
CaramonH <caramonh@yahoo.com>
2024-01-15 12:29:20 -0600

    checking something

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -21,22 +21,16 @@ if auth_type == "auth":
     auth = Auth()
 
 
-@app.before_request
-def before_request():
-    """ Not found and Unauthorized handlers """
-    if auth is None:
-        return
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler """
+    return jsonify({"error": "Not found"}), 404
 
-    excluded_paths = [
-        '/api/v1/status/',
-        '/api/v1/unauthorized/',
-        '/api/v1/forbidden/']
-    if request.path not in excluded_paths and \
-            auth.require_auth(request.path, excluded_paths):
-        if auth.authorization_header(request) is None:
-            abort(401)
-        if auth.current_user(request) is None:
-            abort(403)
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Unauthorized handler """
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
