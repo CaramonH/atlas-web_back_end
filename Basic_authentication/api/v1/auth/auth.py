390df7cfca3e1ed6b976d84d478bc4e4b154b6d2
CaramonH <caramonh@yahoo.com>
2024-01-15 12:15:43 -0600

    Task 5

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -26,7 +26,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """Get the Authorization header from the request"""
-        return None
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Get the current user from the request"""
