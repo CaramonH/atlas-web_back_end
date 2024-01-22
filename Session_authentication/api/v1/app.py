3eec80cb89dcdf372ecbd023860dada3b2dfeec7
CaramonH <caramonh@yahoo.com>
2024-01-22 17:38:22 -0600

    auth import

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -7,6 +7,7 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+from api.v1.app import auth
 import os
 
 app = Flask(__name__)
