82cbc9364d8481ae008f9a0488e0061a7e3ffba5
CaramonH <caramonh@yahoo.com>
2024-01-23 15:26:32 -0600

    pycodestyle except fix

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -4,6 +4,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 import uuid
 
 
@@ -25,13 +26,16 @@ class Auth:
             hashed_password = self._hash_password(password)
             new_user = self._db.add_user(email, hashed_password)
             return new_user
-        
+
     def valid_login(self, email: str, password: str) -> bool:
         try:
             user = self._db.find_user_by(email=email)
-            if user and bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
+            if user and bcrypt.checkpw(
+                    password.encode('utf-8'), user.hashed_password):
                 return True
-        except:
+        except NoResultFound:
+            pass
+        except InvalidRequestError:
             pass
         return False
 
