0f55c972f4b77adfc1cca8f071847f97531df5c5
CaramonH <caramonh@yahoo.com>
2024-01-23 16:17:03 -0600

    Task 14 Logout

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Basic Flask App"""
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 import uuid
 
 from auth import Auth
@@ -43,6 +43,17 @@ def login():
             abort(401, "Incorrect login information")
     except ValueError:
         abort(401, "Incorrect login information")
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """ Log out """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if session_id is None or user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
 
 
 if __name__ == "__main__":
