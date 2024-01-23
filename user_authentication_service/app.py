a6373ab8c43b648734203de1b7269145b7e3d1a6
CaramonH <caramonh@yahoo.com>
2024-01-23 14:51:49 -0600

    test

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 """Basic Flask App"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
-@app.route("/")
-def welcome():
-    return jsonify({"message": "Bienvenue"})
+@app.route('/users', methods=['POST'])
+def users():
+    try:
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = AUTH.register_user(email, password)
+
+        return jsonify({"email": user.email, "message": "user created"}), 200
+
+    except ValueError as e:
+        return jsonify({"message": str(e)}), 400
 
 
 if __name__ == "__main__":
