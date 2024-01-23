5a5c4829bf5a424b68a29e7222f7c1a9562fffa4
CaramonH <caramonh@yahoo.com>
2024-01-23 16:54:46 -0600

    bleh

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -71,6 +71,17 @@ class Auth:
             except (NoResultFound, InvalidRequestError):
                 pass
 
+    def get_reset_password_token(self, email: str) -> str:
+        """ If it exists, generate a UUID and update the user’s reset_token
+            database field. Return the token """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=token)
+        return token
+
 
 def _hash_password(password: str) -> bytes:
     """Hash a password so that it can be stored
