d2810a56ab589e68aa0286c0a94a1f88c674d7ff
CaramonH <caramonh@yahoo.com>
2024-01-23 16:30:21 -0600

    wish there was a main file

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -55,7 +55,7 @@ def login():
 @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
 def logout():
     """ Log out """
-    session_id = request.cookies.get('session_id')
+    session_id = request.cookies.get('session_id', None)
     user = AUTH.get_user_from_session_id(session_id)
     if session_id is None or user is None:
         abort(403)
