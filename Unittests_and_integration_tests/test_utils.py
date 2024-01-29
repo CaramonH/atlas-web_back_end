0e263a501e4f6819a235ca94657342b536b31578
CaramonH <caramonh@yahoo.com>
2024-01-29 12:30:02 -0600

    Task 3

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -24,7 +24,8 @@ class TestAccessNestedMap(unittest.TestCase):
         ({}, ("a",), "Key 'a' not found in nested map"),
         ({"a": 1}, ("a", "b"), "Key 'b' not found in nested map"),
     ])
-    def test_access_nested_map_exception(self, nested_map, path, expected_exception_message):
+    def test_access_nested_map_exception(
+            self, nested_map, path, expected_exception_message):
         """checks if method raises the correct exception"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
@@ -46,6 +47,44 @@ class TestGetJson(unittest.TestCase):
             result = get_json(test_url)
             self.assertEqual(result, test_payload)
             mock_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """ TestMemoize class for utils.py """
+
+    def test_memoize(self):
+        """ Testing the memoize method to return expected"""
+
+        class TestClass:
+            """TestClass"""
+            def __init__(self):
+                """ init method"""
+                self.call_count = 0
+
+            def a_method(self):
+                """ a_method method method method method"""
+                self.call_count += 1
+                return 42
+
+            @memoize
+            def a_property(self):
+                """ a_property method
+                Args:
+                    self (TestClass): the class itself
+                """
+                return self.a_method()
+
+        # create a mock object
+        with patch.object(
+                TestClass, 'a_method', return_value=42) as mocked_method:
+            test_instance = TestClass()
+            result1 = test_instance.a_property
+            result2 = test_instance.a_property
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+
+            mocked_method.assert_called_once()
 
 
 if __name__ == '__main__':
