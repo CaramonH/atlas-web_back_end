0a58909269c49bbb15c12db12e6d2bbcba850fb4
CaramonH <caramonh@yahoo.com>
2024-01-29 11:48:33 -0600

    Task 0

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+"""
+Generic utilities for github org client.
+"""
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map, get_json, memoize
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ TestAccessNestedMap class"""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected_result):
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
