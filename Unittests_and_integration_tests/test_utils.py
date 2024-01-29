f25cf8fe4b6ef1b98617c66cd7749c4beab9a89c
CaramonH <caramonh@yahoo.com>
2024-01-29 12:17:26 -0600

    Task 2

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -4,11 +4,12 @@ Generic utilities for github org client.
 """
 import unittest
 from parameterized import parameterized
+from unittest.mock import patch, Mock
 from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """ TestAccessNestedMap class"""
+    """ TestAccessNestedMap class """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -27,6 +28,25 @@ class TestAccessNestedMap(unittest.TestCase):
         """checks if method raises the correct exception"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """ TestGetJson class for utils.py """
+    @parameterized.expand([
+        ('http://example.com', {'payload': True}),
+        ('http://holberton.io', {'payload': False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """check if the method returns the result as expected"""
+
+        mock = Mock()
+        mock.json.return_value = test_payload
+        with patch('requests.get') as mock_get:
+            mock_get.return_value = mock
+            result = get_json(test_url)
+            self.assertEqual(result, test_payload)
+            mock_get.assert_called_once_with(test_url)
+
 
 if __name__ == '__main__':
     unittest.main()
