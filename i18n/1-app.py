365053cf98681f9ae33c6138634f64c2f9cd1c8c
CaramonH <caramonh@yahoo.com>
2024-01-30 10:24:37 -0600

    Task 2

---
+++ i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""1-app.py"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Babel Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """GET method for index.html"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
