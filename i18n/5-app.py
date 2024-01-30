6f344071267836dc2135acc5dab388867c8957f5
CaramonH <caramonh@yahoo.com>
2024-01-30 12:41:02 -0600

    Task 5

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -1,2 +1,61 @@
 #!/usr/bin/env python3
 """5-app.py"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    """Babel Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+def get_user():
+    """ Returns user dictionary if it exist """
+    user_id = request.args.get('login_as')
+    if user_id:
+        try:
+            user_id = int(user_id)
+            return users.get(user_id)
+        except ValueError:
+            return None
+    return None
+
+
+@app.before_request
+def before_request():
+    """before request"""
+    g.user = get_user()
+
+
+@babel.localeselector
+def get_locale():
+    """Return user preferred locale"""
+    url_locale = request.args.get('locale')
+    if url_locale and url_locale in app.config['LANGUAGES']:
+        return url_locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """GET method for index.html"""
+    from flask_babel import gettext as _
+    return render_template('/3-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
