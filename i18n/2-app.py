74561cbe2811e0744fc3ededf9e6b76397ea5cd1
CaramonH <caramonh@yahoo.com>
2024-01-30 11:44:26 -0600

    decorator

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
-
+@babel.localeselector
 def get_locale():
     """Return user preferred locale"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
