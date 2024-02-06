12ea79484d22237f368bedd926a9d530def87c4c
CaramonH <caramonh@yahoo.com>
2024-02-06 11:54:22 -0600

    Task 1 ?

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,7 +4,7 @@ Module which defines the cache class using Redis
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -17,3 +17,18 @@ class Cache:
         key: str = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable]) -> \
+            Union[str, bytes, int, float]:
+        value = self._redis.get(key)
+        if value is not None and fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        value = self.get(key, str)
+        return value
+
+    def get_int(self, key: str) -> Optional[int]:
+        value = self.get(key, int)
+        return value
