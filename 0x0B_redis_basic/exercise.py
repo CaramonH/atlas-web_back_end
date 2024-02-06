80ccb268acbc6265644fb64d53273132e253b068
CaramonH <caramonh@yahoo.com>
2024-02-06 11:24:56 -0600

    Task 0

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,3 +2,18 @@
 """Writing Strings to Redis.
 Module which defines the cache class using Redis
 """
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """Defines a Redis cache"""
+    def __init__(self) -> None:
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key: str = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
