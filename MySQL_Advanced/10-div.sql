8e4bc1a5a2947089866af4ac78295eed9649d97d
CaramonH <caramonh@yahoo.com>
2024-02-13 13:37:28 -0600

    Task 10 and tests

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -3,14 +3,13 @@
 -- Returns 0 if the second number is equal to 0
 DELIMIER //
 
-CREATE FUNCTION SafeDiv (a INT, b INT)
-RETURNS FLOAT
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
 BEGIN
     IF b = 0 THEN
         RETURN 0;
+    ELSE
+        RETURN a / b;
     END IF;
-    RETURN a / b;
-END
-//
+END //
 
 DELIMITER ;
