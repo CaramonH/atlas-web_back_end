fd92f27e151fbc502fb944ab091e841329d47ea5
CaramonH <caramonh@yahoo.com>
2024-02-21 10:01:06 -0600

    Task 2

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -4,8 +4,25 @@ const fs = require('fs');
 
 function countStudents(path) {
   if (!fs.existsSync(path)) {
-		throw new Error('Cannot load the database');
-	}
+    throw new Error('Cannot load the database');
+  }
 
-	
+  const data = fs.readFileSync(path, 'utf8');
+  const students = data.trim().split('\n').slice(1);
+  const studentsByField = {};
+
+  students.forEach((student) => {
+    const [firstName,, , field] = student.split(',');
+    if (!studentsByField[field]) studentsByField[field] = [];
+    studentsByField[field].push(firstName);
+  });
+
+  const totalStudents = students.length;
+  console.log(`Number of students: ${totalStudents}`);
+
+  Object.entries(studentsByField).forEach(([field, names]) => {
+    console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+  });
 }
+
+module.exports = countStudents;
