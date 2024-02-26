958253cb50c18067ddab447a48c4ad214749f622
CaramonH <caramonh@yahoo.com>
2024-02-26 10:11:49 -0600

    Task 0

---
+++ unittests_in_js/0-calcul.js
@@ -0,0 +1,8 @@
+// Create a function named calculateNumber
+// Accepts two arguments, rounds, and returns the sum
+
+function calculateNumber(a, b) {
+    return Math.round(a) + Math.round(b);
+}
+
+module.exports = calculateNumber;
